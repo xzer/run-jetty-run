0f4f094f4625641ae276f55a6ef187f16a8770de
tonylovejava@gmail.com <tonylovejava@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2011-03-15 05:23:37 +0000

    Fix for Issue #53  , RunJettyRun support in Beanshell.

--- trunk/bootstrap/src/runjettyrun/ProjectClassLoader.java
+++ trunk/bootstrap/src/runjettyrun/ProjectClassLoader.java
@@ -1,14 +1,14 @@
 /*
  * $Id$
  * $HeadURL$
- * 
+ *
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -28,7 +28,7 @@ import org.mortbay.resource.Resource;
  * Uses the provided class path ONLY, rather than also supporting the adding of
  * the jars in the WEB-INF/lib directory, and the adding of the classes in the
  * WEB-INF/classes directory.
- * 
+ *
  * @author jsynge
  */
 public class ProjectClassLoader extends WebAppClassLoader {
@@ -44,10 +44,10 @@ public class ProjectClassLoader extends WebAppClassLoader {
 
     /*
      * As reported in these bugs:
-     * 
+     *
      *          http://code.google.com/p/run-jetty-run/issues/detail?id=25
      *          http://code.google.com/p/run-jetty-run/issues/detail?id=26
-     * 
+     *
      * the path separator defined by Java (java.io.File.pathSeparator)
      * (and used by the run-jetty-run plug-in) may not match the one used by
      * Jetty (which is expects it to be either a comma or a semi-colon).
@@ -70,6 +70,21 @@ public class ProjectClassLoader extends WebAppClassLoader {
     }
 
     initialized = true;
+  }
+
+  /**
+   * code fix for a strange case with Beanshell suuport ,
+   * see Issue #53 for more detail
+   * http://code.google.com/p/run-jetty-run/issues/detail?id=53
+   */
+  @Override
+  public Class loadClass(String name) throws ClassNotFoundException
+  {
+	  try{
+		  return loadClass(name, false);
+	  }catch(NoClassDefFoundError e){
+		  throw new ClassNotFoundException(name);
+	  }
   }
 
   @Override
