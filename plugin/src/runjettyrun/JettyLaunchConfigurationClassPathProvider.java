2d1dc3f6b033d707a9b20c6afde84a41b0b07510
tonylovejava <tonylovejava@gmail.com>
2011-03-21 03:57:57 +0000

    Using container to manager Bootstrap and Jetty6 Bundle for Issue #18 .

--- plugin/src/runjettyrun/JettyLaunchConfigurationClassPathProvider.java
+++ plugin/src/runjettyrun/JettyLaunchConfigurationClassPathProvider.java
@@ -1,14 +1,14 @@
 /*
  * $Id$
  * $HeadURL$
- * 
+ *
  * ==============================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -17,34 +17,26 @@
  */
 package runjettyrun;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.jdt.core.IJavaModel;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.launching.StandardClasspathProvider;
-import org.osgi.framework.Bundle;
 
+import runjettyrun.container.RunJettyRunContainerClasspathEntry;
+import runjettyrun.container.RunJettyRunContainerRuntimeClasspathEntry;
 import runjettyrun.utils.RunJettyRunClasspathUtil;
 
+@SuppressWarnings("restriction")
 public class JettyLaunchConfigurationClassPathProvider extends
     StandardClasspathProvider {
 
@@ -73,53 +65,13 @@ public class JettyLaunchConfigurationClassPathProvider extends
   private IRuntimeClasspathEntry[] addJettyAndBootstrap(
       IRuntimeClasspathEntry[] existing, ILaunchConfiguration config) {
 
+
     List<IRuntimeClasspathEntry> entries = new ArrayList<IRuntimeClasspathEntry>();
     entries.addAll(Arrays.asList(existing));
-    Bundle bundle = Plugin.getDefault().getBundle();
-    URL installUrl = bundle.getEntry("/");
-
-    addRelativeArchiveEntry(entries, installUrl, "run-jetty-run-bootstrap");
-    addRelativeArchiveEntry(entries, installUrl, "jetty-"
-        + Plugin.JETTY_VERSION);
-    addRelativeArchiveEntry(entries, installUrl, "jetty-util-"
-        + Plugin.JETTY_VERSION);
-    addRelativeArchiveEntry(entries, installUrl, "jetty-management-"
-        + Plugin.JETTY_VERSION);
-    addRelativeArchiveEntry(entries, installUrl, "servlet-api-2.5-20081211");
-    addRelativeArchiveEntry(entries, installUrl, "jsp-api-2.1");
-    addRelativeArchiveEntry(entries, installUrl, "jsp-2.1");
-    addRelativeArchiveEntry(entries, installUrl, "core-3.1.1");
-
+    entries.add(new RunJettyRunContainerRuntimeClasspathEntry(Plugin.CONTAINER_RJR_BOOTSTRAP,IRuntimeClasspathEntry.USER_CLASSES));
+    entries.add(new RunJettyRunContainerRuntimeClasspathEntry(Plugin.CONTAINER_RJR_JETTY6,IRuntimeClasspathEntry.USER_CLASSES));
     return entries.toArray(new IRuntimeClasspathEntry[entries.size()]);
-  }
 
-  private void addRelativeArchiveEntry(List<IRuntimeClasspathEntry> entries,
-      URL installUrl, String libJarName) {
-
-    try {
-      String relativePath = "lib/" + libJarName + ".jar";
-      URL bundleUrl = new URL(installUrl, relativePath);
-      addArchiveEntry(entries, bundleUrl);
-      return;
-    } catch (MalformedURLException e) {
-      Plugin.logError(e);
-      return;
-    }
-  }
-
-  private void addArchiveEntry(List<IRuntimeClasspathEntry> entries,
-      URL bundleUrl) {
-
-    try {
-      URL fileUrl = FileLocator.toFileURL(bundleUrl);
-      IRuntimeClasspathEntry rcpe = JavaRuntime
-          .newArchiveRuntimeClasspathEntry(new Path(fileUrl.getFile()));
-      entries.add(rcpe);
-      return;
-    } catch (IOException e) {
-      Plugin.logError(e);
-      return;
-    }
   }
 
 
@@ -127,9 +79,9 @@ public class JettyLaunchConfigurationClassPathProvider extends
    * James Synge: overriding so that I can block the inclusion of external
    * libraries that should be found in WEB-INF/lib, and shouldn't be on the
    * JVM's class path.
-   * 
+   *
    * (non-Javadoc)
-   * 
+   *
    * @see
    * org.eclipse.jdt.launching.StandardClasspathProvider#resolveClasspath(org
    * .eclipse.jdt.launching.IRuntimeClasspathEntry[],
@@ -153,24 +105,6 @@ public class JettyLaunchConfigurationClassPathProvider extends
           .resolveRuntimeClasspathEntry(entry, configuration);
       all.addAll(Arrays.asList(resolved));
 
-      // for (int j = 0; j < resolved.length; j++) {
-      // IRuntimeClasspathEntry resolvedEntry = resolved[j];
-      // if (isProject)
-      // {
-      // String location = resolvedEntry.getLocation();
-      // IResource resource = resolvedEntry.getResource();
-      // int classpathProperty = resolvedEntry.getClasspathProperty();
-      // if (classpathProperty == IRuntimeClasspathEntry.USER_CLASSES)
-      // {
-      // if (location != null)
-      // {
-      // if (resource == null)
-      // continue; // External library; skip it.
-      // }
-      // }
-      // }
-      // all.add(resolvedEntry);
-      // }
     }
 
     IRuntimeClasspathEntry[] resolvedClasspath = all
---
+++ plugin/src/runjettyrun/container/RunJettyRunContainerClasspathEntry.java
@@ -0,0 +1,27 @@
+package runjettyrun.container;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.internal.core.ClasspathAttribute;
+import org.eclipse.jdt.internal.core.ClasspathEntry;
+
+@SuppressWarnings("restriction")
+public class RunJettyRunContainerClasspathEntry extends ClasspathEntry {
+
+	public RunJettyRunContainerClasspathEntry(String containerName) {
+	    this(containerName,new ClasspathAttribute[0]);
+	}
+
+	public RunJettyRunContainerClasspathEntry(String containerName,List<ClasspathAttribute> attribute) {
+	    this(containerName,attribute.toArray(new ClasspathAttribute[0]));
+	}
+
+	public RunJettyRunContainerClasspathEntry(String containerName,ClasspathAttribute[] attribute) {
+	    super(0, ClasspathEntry.CPE_CONTAINER,
+	    		new Path(containerName), new IPath[0], new IPath[0], null, null, null, null, false, null, false,
+	    		attribute);
+	}
+
+}
