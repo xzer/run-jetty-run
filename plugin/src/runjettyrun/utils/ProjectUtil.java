6042c6fdac867df6e8b273f0b748fab9ed0e9b36
tonylovejava <tonylovejava@gmail.com>
2011-05-08 11:06:39 +0000

    Committed for Issue 57 running the plugin directly from the run as debug as menu

--- plugin/src/runjettyrun/utils/ProjectUtil.java
+++ plugin/src/runjettyrun/utils/ProjectUtil.java
@@ -5,6 +5,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IEditorInput;
@@ -13,56 +14,97 @@ import org.eclipse.ui.part.FileEditorInput;
 
 public class ProjectUtil {
 	public static String MAVEN_NATURE_ID = "org.maven.ide.eclipse.maven2Nature";
-	
-	public static IResource getSelectedResource(IWorkbenchWindow window){
-		
-		ISelection selection = window.getSelectionService().getSelection();
-		
-		if(selection instanceof TreeSelection){//could be project explorer
+
+	public static IResource getSelectedResource(ISelection selection) {
+
+		if (selection instanceof TreeSelection) {// could be project explorer
 			Object first = ((TreeSelection) selection).getFirstElement();
-			if(first instanceof IFile)	return (IFile)first;
+			if (first instanceof IResource)
+				return (IResource) first;
+			else if(first instanceof IJavaProject)
+					return ((IJavaProject)first).getResource();
+			else if(first instanceof IProject){
+				try {
+					return ((IProject)first).members()[0];
+				} catch (CoreException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
 		}
-		
-		IEditorInput editorinput = window.getActivePage().getActiveEditor().getEditorInput();
-		FileEditorInput fileEditorInput = (FileEditorInput) editorinput.getAdapter(FileEditorInput.class);
-		if( fileEditorInput == null || fileEditorInput.getFile()== null){
+
+		return null;
+	}
+
+	public static IResource getSelectedResource(IWorkbenchWindow window) {
+
+		ISelection selection = window.getSelectionService().getSelection();
+
+		IResource ir = getSelectedResource(selection);
+		if (ir != null)
+			return ir;
+
+		IEditorInput editorinput = window.getActivePage().getActiveEditor()
+				.getEditorInput();
+		FileEditorInput fileEditorInput = (FileEditorInput) editorinput
+				.getAdapter(FileEditorInput.class);
+		if (fileEditorInput == null || fileEditorInput.getFile() == null) {
 			return null;
 		}
 		return fileEditorInput.getFile();
 	}
-	public static IProject getProject(IWorkbenchWindow window){
-		IEditorInput editorinput = window.getActivePage().getActiveEditor().getEditorInput();
-		FileEditorInput fileEditorInput = (FileEditorInput) editorinput.getAdapter(FileEditorInput.class);
 
-		if( fileEditorInput == null || fileEditorInput.getFile()== null){
+	/**
+	 *
+	 * @param editorinput
+	 * @return null if not found
+	 */
+	public static IFile getFile(IEditorInput editorinput ) {
+		FileEditorInput fileEditorInput = (FileEditorInput) editorinput
+				.getAdapter(FileEditorInput.class);
+
+		if (fileEditorInput == null || fileEditorInput.getFile() == null) {
+			return null;
+		}
+		return fileEditorInput.getFile();
+	}
+
+	public static IProject getProject(IWorkbenchWindow window) {
+		IEditorInput editorinput = window.getActivePage().getActiveEditor()
+				.getEditorInput();
+		FileEditorInput fileEditorInput = (FileEditorInput) editorinput
+				.getAdapter(FileEditorInput.class);
+
+		if (fileEditorInput == null || fileEditorInput.getFile() == null) {
 			return null;
 		}
 		return fileEditorInput.getFile().getProject();
 	}
 
-	public static IProject getProject(IEditorInput editorinput){
-		FileEditorInput fileEditorInput = (FileEditorInput) editorinput.getAdapter(FileEditorInput.class);
+	public static IProject getProject(IEditorInput editorinput) {
+		FileEditorInput fileEditorInput = (FileEditorInput) editorinput
+				.getAdapter(FileEditorInput.class);
 
-		if( fileEditorInput == null || fileEditorInput.getFile()== null){
+		if (fileEditorInput == null || fileEditorInput.getFile() == null) {
 			return null;
 		}
 		return fileEditorInput.getFile().getProject();
 	}
 
-	public static IContainer getWebappFolder(IProject project,String webappdir){
-		
+	public static IContainer getWebappFolder(IProject project, String webappdir) {
+
 		IContainer folder = null;
-		if("/".equals(webappdir))
+		if ("/".equals(webappdir))
 			folder = project;
 		else
 			folder = project.getFolder(webappdir);
-	
+
 		return folder;
 	}
-	
-	public static  boolean isMavenProject(IProject project) {
+
+	public static boolean isMavenProject(IProject project) {
 		try {
-			return project!=null && project.hasNature(MAVEN_NATURE_ID);
+			return project != null && project.hasNature(MAVEN_NATURE_ID);
 		} catch (CoreException e) {
 			return false;
 		}
