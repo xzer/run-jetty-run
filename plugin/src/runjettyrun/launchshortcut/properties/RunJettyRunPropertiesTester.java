91440deb38659da32e3173141a9f785ba3a740eb
tonylovejava <tonylovejava@gmail.com>
2011-05-16 17:14:32 +0000

    Issue 59, Add enablement support .

---
+++ plugin/src/runjettyrun/launchshortcut/properties/RunJettyRunPropertiesTester.java
@@ -0,0 +1,21 @@
+package runjettyrun.launchshortcut.properties;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IResource;
+
+import runjettyrun.RunJettyRunTab;
+
+public class RunJettyRunPropertiesTester extends PropertyTester
+{
+
+	public boolean test(Object receiver, String property, Object[] args,
+			Object expectedValue) {
+		if(!(receiver instanceof IResource ))
+			return false;
+
+		IResource ir = (IResource) receiver;
+
+		return RunJettyRunTab.isWebappProject(ir.getProject());
+	}
+
+}
