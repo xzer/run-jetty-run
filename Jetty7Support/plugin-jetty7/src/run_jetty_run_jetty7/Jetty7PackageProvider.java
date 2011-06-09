ce3949c4ebfd3fadf8a79d2b8219f9494dd608a8
tonylovejava@gmail.com <tonylovejava@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2011-06-09 15:30:00 +0000

    commit for Issue 40 .

---
+++ Jetty7Support/plugin-jetty7/src/run_jetty_run_jetty7/Jetty7PackageProvider.java
@@ -0,0 +1,55 @@
+package run_jetty_run_jetty7;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
+
+import runjettyrun.extensions.IJettyPackageProvider;
+import runjettyrun.utils.ProjectUtil;
+
+public class Jetty7PackageProvider implements IJettyPackageProvider {
+
+	public static final String VERSION = "Jetty 7.4.2.v20110526";
+	public IRuntimeClasspathEntry[] getPackage(String version, int type) {
+		try {
+			if (type == TYPE_JETTY_BUNDLE) {
+
+				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
+						"lib/");
+
+			} else if (type == TYPE_UTIL) {
+				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
+				"jndilib/");
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+
+	public boolean acceptType(int type) {
+		return ( type == TYPE_JETTY_BUNDLE) || (type == TYPE_UTIL) ;
+	}
+
+	public String getJettyVersion() {
+		return VERSION;
+	}
+
+	public boolean accpet(String ver) {
+		return VERSION.equals(ver);
+	}
+
+	public String getName() {
+		return VERSION;
+	}
+
+	public String getText() {
+		return VERSION;
+	}
+
+}
