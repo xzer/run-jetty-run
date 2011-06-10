9391d517c9eebe330fd6c1059a21f8f35b9af03e
tonylovejava@gmail.com <tonylovejava@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2011-06-10 10:36:53 +0000

    fine tune for jetty7,jetty8 support
    git-svn-id: http://run-jetty-run.googlecode.com/svn@163 88211e8e-dc41-0410-8f0f-a3d8d629f5a5

--- bootstrap-jetty7/src/runjettyrun/Bootstrap.java
+++ bootstrap-jetty7/src/runjettyrun/Bootstrap.java
@@ -133,6 +133,7 @@ public class Bootstrap {
 
 	private static void initConnnector(Server server, Configs configObj) {
 		SelectChannelConnector connector = new SelectChannelConnector();
+		connector.setHost("127.0.0.1");
 		connector.setPort(configObj.getPort());
 
 		if (configObj.getEnablessl() && configObj.getSslport() != null)
