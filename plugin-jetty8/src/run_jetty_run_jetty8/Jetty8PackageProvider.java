5c8515add338759e6bca3aea7a3ce9220056d83b
tonylovejava@gmail.com <tonylovejava@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2011-07-07 00:24:09 +0000

    Fix issue 78 : RJR failing to include required jars in classpath
    git-svn-id: http://run-jetty-run.googlecode.com/svn@222 88211e8e-dc41-0410-8f0f-a3d8d629f5a5

--- plugin-jetty8/src/run_jetty_run_jetty8/Jetty8PackageProvider.java
+++ plugin-jetty8/src/run_jetty_run_jetty8/Jetty8PackageProvider.java
@@ -17,11 +17,44 @@ public class Jetty8PackageProvider implements IJettyPackageProvider {
 			if (type == TYPE_JETTY_BUNDLE) {
 
 				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
-						"lib/");
+						new String[]{
+					"lib/com.sun.el_2.2.0.v201105051105.jar",
+					"lib/javax.el_2.2.0.v201105051105.jar",
+					"lib/javax.servlet.jsp.jstl_1.2.0.v201004190952.jar",
+					"lib/javax.servlet.jsp_2.2.0.v201103241009.jar",
+					"lib/jetty-ajp-8.0.0.M3.jar",
+					"lib/jetty-annotations-8.0.0.M3.jar",
+					"lib/jetty-client-8.0.0.M3.jar",
+					"lib/jetty-continuation-8.0.0.M3.jar",
+					"lib/jetty-deploy-8.0.0.M3.jar",
+					"lib/jetty-http-8.0.0.M3.jar",
+					"lib/jetty-io-8.0.0.M3.jar",
+					"lib/jetty-jmx-8.0.0.M3.jar",
+					"lib/jetty-jndi-8.0.0.M3.jar",
+					"lib/jetty-overlay-deployer-8.0.0.M3.jar",
+					"lib/jetty-plus-8.0.0.M3.jar",
+					"lib/jetty-policy-8.0.0.M3.jar",
+					"lib/jetty-rewrite-8.0.0.M3.jar",
+					"lib/jetty-security-8.0.0.M3.jar",
+					"lib/jetty-server-8.0.0.M3.jar",
+					"lib/jetty-servlet-8.0.0.M3.jar",
+					"lib/jetty-servlets-8.0.0.M3.jar",
+					"lib/jetty-util-8.0.0.M3.jar",
+					"lib/jetty-webapp-8.0.0.M3.jar",
+					"lib/jetty-websocket-8.0.0.M3.jar",
+					"lib/jetty-xml-8.0.0.M3.jar",
+					"lib/jsp-impl-2.2.2.b05.0.jar",
+					"lib/org.apache.taglibs.standard.glassfish_1.2.0.v201004190952.jar",
+					"lib/run-jetty-run-bootstrap-jetty8.jar",
+					"lib/servlet-api-3.0.jar"
+				});
 
 			} else if (type == TYPE_UTIL) {
 				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
-				"jndilib/");
+				 new String[]{
+					"jndilib/javax.activation_1.1.0.v201105071233.jar",
+					"jndilib/javax.mail.glassfish_1.4.1.v201005082020.jar"
+				});
 			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
