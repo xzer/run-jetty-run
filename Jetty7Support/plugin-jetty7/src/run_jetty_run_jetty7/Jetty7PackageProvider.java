b2b2127a34e93cb8435b0dbec07283554c7f132a
tonylovejava@gmail.com <tonylovejava@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2011-07-07 00:24:09 +0000

    Fix issue 78 : RJR failing to include required jars in classpath

--- Jetty7Support/plugin-jetty7/src/run_jetty_run_jetty7/Jetty7PackageProvider.java
+++ Jetty7Support/plugin-jetty7/src/run_jetty_run_jetty7/Jetty7PackageProvider.java
@@ -16,11 +16,46 @@ public class Jetty7PackageProvider implements IJettyPackageProvider {
 			if (type == TYPE_JETTY_BUNDLE) {
 
 				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
-						"lib/");
+						new String[]{
+					"lib/com.sun.el_1.0.0.v201004190952.jar",
+					"lib/ecj-3.6.jar",
+					"lib/javax.el_2.1.0.v201004190952.jar",
+					"lib/javax.servlet.jsp.jstl_1.2.0.v201004190952.jar",
+					"lib/javax.servlet.jsp_2.1.0.v201004190952.jar",
+					"lib/jetty-ajp-7.4.2.v20110526.jar",
+					"lib/jetty-annotations-7.4.2.v20110526.jar",
+					"lib/jetty-client-7.4.2.v20110526.jar",
+					"lib/jetty-continuation-7.4.2.v20110526.jar",
+					"lib/jetty-deploy-7.4.2.v20110526.jar",
+					"lib/jetty-http-7.4.2.v20110526.jar",
+					"lib/jetty-io-7.4.2.v20110526.jar",
+					"lib/jetty-jmx-7.4.2.v20110526.jar",
+					"lib/jetty-jndi-7.4.2.v20110526.jar",
+					"lib/jetty-jsp-2.1-7.4.2.v20110526.jar",
+					"lib/jetty-overlay-deployer-7.4.2.v20110526.jar",
+					"lib/jetty-plus-7.4.2.v20110526.jar",
+					"lib/jetty-policy-7.4.2.v20110526.jar",
+					"lib/jetty-rewrite-7.4.2.v20110526.jar",
+					"lib/jetty-security-7.4.2.v20110526.jar",
+					"lib/jetty-server-7.4.2.v20110526.jar",
+					"lib/jetty-servlet-7.4.2.v20110526.jar",
+					"lib/jetty-servlets-7.4.2.v20110526.jar",
+					"lib/jetty-util-7.4.2.v20110526.jar",
+					"lib/jetty-webapp-7.4.2.v20110526.jar",
+					"lib/jetty-websocket-7.4.2.v20110526.jar",
+					"lib/jetty-xml-7.4.2.v20110526.jar",
+					"lib/org.apache.jasper.glassfish_2.1.0.v201007080150.jar",
+					"lib/org.apache.taglibs.standard.glassfish_1.2.0.v201004190952.jar",
+					"lib/run-jetty-run-bootstrap-jetty7.jar",
+					"lib/servlet-api-2.5.jar"
+				});
 
 			} else if (type == TYPE_UTIL) {
 				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
-				"jndilib/");
+				 new String[]{
+					"jndilib/javax.activation_1.1.0.v201005080500.jar",
+					"jndilib/javax.mail.glassfish_1.4.1.v201005082020.jar"
+				});
 			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
