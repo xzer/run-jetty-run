1e5e0f6b9bc4f1462782d44c75c099a3becf2409
tonylovejava@gmail.com <tonylovejava@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2011-12-15 00:48:05 +0000

    For Issue 88 1.Change the implementation for test-classes 2.Now scan list default not scaning test-classes
    git-svn-id: http://run-jetty-run.googlecode.com/svn@273 88211e8e-dc41-0410-8f0f-a3d8d629f5a5

--- bootstrap-jetty8/src/runjettyrun/ProjectClassLoader.java
+++ bootstrap-jetty8/src/runjettyrun/ProjectClassLoader.java
@@ -61,6 +61,7 @@ public class ProjectClassLoader extends WebAppClassLoader {
 		 * class, one at a time.
 		 */
 	    if(projectClassPath!=null){
+	    	StringBuffer excludedString = new StringBuffer();
 		    String[] tokens = projectClassPath.split(String.valueOf(File.pathSeparatorChar));
 		    for(String entry:tokens){
 		    	String path = entry;
@@ -68,13 +69,14 @@ public class ProjectClassLoader extends WebAppClassLoader {
 		    		path = path.substring(3);
 		    	}
 		    	if(entry.startsWith("-n-")){
-		    		System.err.println("ProjectClassLoader excluded entry="+ path);
+		    		excludedString.append("Excluded entry="+ path+" (Not loaded.)\n");
 		    	}else{
 			    	if (logger) System.err.println("ProjectClassLoader: entry="+ path);
 			    	classpaths.add(path);
 		    		super.addClassPath( path);
 		    	}
 		    }
+		    System.err.print(excludedString.toString());
 	    }
 
 		initialized = true;
