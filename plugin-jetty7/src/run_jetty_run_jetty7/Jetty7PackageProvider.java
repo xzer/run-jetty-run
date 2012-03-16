16d53981d558e44095df8734db47722df09b57e1
tonylovejava@gmail.com <tonylovejava@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2012-03-16 03:48:08 +0000

    upgrade to jetty lastest version
    git-svn-id: http://run-jetty-run.googlecode.com/svn@323 88211e8e-dc41-0410-8f0f-a3d8d629f5a5

--- plugin-jetty7/src/run_jetty_run_jetty7/Jetty7PackageProvider.java
+++ plugin-jetty7/src/run_jetty_run_jetty7/Jetty7PackageProvider.java
@@ -1,5 +1,6 @@
 package run_jetty_run_jetty7;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
@@ -22,38 +23,38 @@ public class Jetty7PackageProvider implements IJettyPackageProvider {
 					"lib/javax.el_2.1.0.v201004190952.jar",
 					"lib/javax.servlet.jsp.jstl_1.2.0.v201004190952.jar",
 					"lib/javax.servlet.jsp_2.1.0.v201004190952.jar",
-					"lib/jetty-ajp-7.5.1.v20110908.jar",
-					"lib/jetty-annotations-7.5.1.v20110908.jar",
-					"lib/jetty-client-7.5.1.v20110908.jar",
-					"lib/jetty-continuation-7.5.1.v20110908.jar",
-					"lib/jetty-deploy-7.5.1.v20110908.jar",
-					"lib/jetty-http-7.5.1.v20110908.jar",
-					"lib/jetty-io-7.5.1.v20110908.jar",
-					"lib/jetty-jmx-7.5.1.v20110908.jar",
-					"lib/jetty-jndi-7.5.1.v20110908.jar",
-					"lib/jetty-overlay-deployer-7.5.1.v20110908.jar",
-					"lib/jetty-plus-7.5.1.v20110908.jar",
-					"lib/jetty-policy-7.5.1.v20110908.jar",
-					"lib/jetty-rewrite-7.5.1.v20110908.jar",
-					"lib/jetty-security-7.5.1.v20110908.jar",
-					"lib/jetty-server-7.5.1.v20110908.jar",
-					"lib/jetty-servlet-7.5.1.v20110908.jar",
-					"lib/jetty-servlets-7.5.1.v20110908.jar",
-					"lib/jetty-util-7.5.1.v20110908.jar",
-					"lib/jetty-webapp-7.5.1.v20110908.jar",
-					"lib/jetty-websocket-7.5.1.v20110908.jar",
-					"lib/jetty-xml-7.5.1.v20110908.jar",
+					"lib/jetty-ajp-7.6.2.v20120308.jar",
+					"lib/jetty-annotations-7.6.2.v20120308.jar",
+					"lib/jetty-client-7.6.2.v20120308.jar",
+					"lib/jetty-continuation-7.6.2.v20120308.jar",
+					"lib/jetty-deploy-7.6.2.v20120308.jar",
+					"lib/jetty-http-7.6.2.v20120308.jar",
+					"lib/jetty-io-7.6.2.v20120308.jar",
+					"lib/jetty-jmx-7.6.2.v20120308.jar",
+					"lib/jetty-jndi-7.6.2.v20120308.jar",
+					"lib/jetty-overlay-deployer-7.6.2.v20120308.jar",
+					"lib/jetty-plus-7.6.2.v20120308.jar",
+					"lib/jetty-policy-7.6.2.v20120308.jar",
+					"lib/jetty-rewrite-7.6.2.v20120308.jar",
+					"lib/jetty-security-7.6.2.v20120308.jar",
+					"lib/jetty-server-7.6.2.v20120308.jar",
+					"lib/jetty-servlet-7.6.2.v20120308.jar",
+					"lib/jetty-servlets-7.6.2.v20120308.jar",
+					"lib/jetty-util-7.6.2.v20120308.jar",
+					"lib/jetty-webapp-7.6.2.v20120308.jar",
+					"lib/jetty-websocket-7.6.2.v20120308.jar",
+					"lib/jetty-xml-7.6.2.v20120308.jar",
 					"lib/org.apache.jasper.glassfish_2.1.0.v201007080150.jar",
 					"lib/org.apache.taglibs.standard.glassfish_1.2.0.v201004190952.jar",
 					"lib/run-jetty-run-bootstrap-jetty7.jar",
-					"lib/servlet-api-2.5.jar"
+					"lib/servlet-api-2.5.jar",
 				});
 
 			} else if (type == TYPE_UTIL) {
 				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
 				 new String[]{
 					"jndilib/javax.activation_1.1.0.v201005080500.jar",
-					"jndilib/javax.mail.glassfish_1.4.1.v201005082020.jar"
+					"jndilib/javax.mail.glassfish_1.4.1.v201005082020.jar",
 				});
 			}
 		} catch (MalformedURLException e) {
@@ -85,5 +86,15 @@ public class Jetty7PackageProvider implements IJettyPackageProvider {
 	public String getText() {
 		return VERSION;
 	}
+	public static void main(String[] args) {
+		//We have to update the list manually since I still didn't know how to fetch all the files in a folder in OSGi.
 
+		for(File f :new File("lib").listFiles()){
+			System.out.println("\"lib/"+f.getName()+"\",");
+		}
+
+		for(File f :new File("jndilib").listFiles()){
+			System.out.println("\"jndilib/"+f.getName()+"\",");
+		}
+	}
 }
