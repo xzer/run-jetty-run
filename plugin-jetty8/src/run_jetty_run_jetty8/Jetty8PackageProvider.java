50e808597c60b2d73ec0362fde0acca478e70a20
fcoffee@gmail.com <fcoffee@gmail.com@88211e8e-dc41-0410-8f0f-a3d8d629f5a5>
2013-04-29 18:20:08 +0000

    a better way to fix issue 78
    git-svn-id: http://run-jetty-run.googlecode.com/svn@371 88211e8e-dc41-0410-8f0f-a3d8d629f5a5

--- plugin-jetty8/src/run_jetty_run_jetty8/Jetty8PackageProvider.java
+++ plugin-jetty8/src/run_jetty_run_jetty8/Jetty8PackageProvider.java
@@ -5,6 +5,7 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
 import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
+import org.osgi.framework.Bundle;
 
 import runjettyrun.extensions.IJettyPackageProvider;
 import runjettyrun.utils.ProjectUtil;
@@ -15,53 +16,15 @@ public class Jetty8PackageProvider implements IJettyPackageProvider {
 
 	public IRuntimeClasspathEntry[] getPackage(String version, int type) {
 		try {
+			Bundle bundle = Activator.getDefault().getBundle();
 			if (type == TYPE_JETTY_BUNDLE) {
 
-				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
-						new String[]{
-					"lib/com.sun.el-2.2.0.v201108011116.jar",
-					"lib/javax.annotation_1.1.0.v201105051105.jar",
-					"lib/javax.el-2.2.0.v201108011116.jar",
-					"lib/javax.servlet.jsp-2.2.0.v201112011158.jar",
-					"lib/javax.servlet.jsp.jstl-1.2.0.v201105211821.jar",
-					"lib/jetty-ajp-8.1.8.v20121106.jar",
-					"lib/jetty-annotations-8.1.8.v20121106.jar",
-					"lib/jetty-client-8.1.8.v20121106.jar",
-					"lib/jetty-continuation-8.1.8.v20121106.jar",
-					"lib/jetty-deploy-8.1.8.v20121106.jar",
-					"lib/jetty-http-8.1.8.v20121106.jar",
-					"lib/jetty-io-8.1.8.v20121106.jar",
-					"lib/jetty-jmx-8.1.8.v20121106.jar",
-					"lib/jetty-jndi-8.1.8.v20121106.jar",
-					"lib/jetty-overlay-deployer-8.1.8.v20121106.jar",
-					"lib/jetty-plus-8.1.8.v20121106.jar",
-					"lib/jetty-policy-8.1.8.v20121106.jar",
-					"lib/jetty-rewrite-8.1.8.v20121106.jar",
-					"lib/jetty-security-8.1.8.v20121106.jar",
-					"lib/jetty-server-8.1.8.v20121106.jar",
-					"lib/jetty-servlet-8.1.8.v20121106.jar",
-					"lib/jetty-servlets-8.1.8.v20121106.jar",
-					"lib/jetty-util-8.1.8.v20121106.jar",
-					"lib/jetty-webapp-8.1.8.v20121106.jar",
-					"lib/jetty-websocket-8.1.8.v20121106.jar",
-					"lib/jetty-xml-8.1.8.v20121106.jar",
-					"lib/jsp-impl-2.2.2.b05.0.jar",
-					"lib/org.apache.jasper.glassfish-2.2.2.v201112011158.jar",
-					"lib/org.apache.taglibs.standard.glassfish-1.2.0.v201112081803.jar",
-					"lib/org.objectweb.asm_3.3.1.v201101071600.jar",
-					"lib/run-jetty-run-bootstrap-jetty8.jar",
-					"lib/servlet-api-3.0.jar",
-					"lib/spdy-core-8.1.8.v20121106.jar",
-					"lib/spdy-jetty-8.1.8.v20121106.jar",
-					"lib/spdy-jetty-http-8.1.8.v20121106.jar"
-				});
+				return ProjectUtil.getLibs(bundle,
+						ProjectUtil.getJarFilesIn(bundle, "lib"));
 
 			} else if (type == TYPE_UTIL) {
-				return ProjectUtil.getLibs(Activator.getDefault().getBundle(),
-				 new String[]{
-					"jndilib/javax.activation_1.1.0.v201105071233.jar",
-					"jndilib/javax.mail.glassfish_1.4.1.v201005082020.jar",
-				});
+				return ProjectUtil.getLibs(bundle,
+						ProjectUtil.getJarFilesIn(bundle, "jndilib"));
 			}
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
