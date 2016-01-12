a0c37ab0d71afb4bc44c57fc51cfa5a0da43434a
xzer <e.ryu@astamuse.co.jp>
2016-01-12 11:15:10 +0900

    ready for 1.3.4 release

--- fixVersion.sh
+++ fixVersion.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$1
+
+echo $1 > currentVersion
---
+++ release.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+sitefolder=$1
+currentFolder=`pwd`
+
+if [ "$sitefolder"X = "X" ]
+then
+    echo $0 '<site folder>'
+    exit 1
+fi
+
+# mvn clean package
+
+if [ $? -ne 0 ]
+then
+    echo
+    echo
+    echo mvn failed.
+    exit 1
+fi
+
+currentVersion=`head -n1 currentVersion`
+
+head -n1 currentVersion | grep "\-SNAPSHOT"
+
+if [ $? -eq 0 ]
+then
+    copyTarget=nightly
+else
+    copyTarget=current
+fi
+
+echo copyTarget is $copyTarget
+
+cd $sitefolder/updatesite
+rm -rf $copyTarget
+cp -R $currentFolder/Runjettyrun/updatesite-nightly/target/repository .
+mv repository $copyTarget
+
+echo copied files to $sitefolder/updatesite/$copyTarget
+
+if [ $copyTarget == "current" ]
+then
+    cp -R $copyTarget ./archive/.
+    mv ./archive/$copyTarget ./archive/$currentVersion
+    echo archied file to $sitefolder/updatesite/archive/$currentVersion
+fi
+
+echo release finished.
+
+
+
+
